c78cb8674736faec993291c084dda2219ceb4948
Kim Bergstrom <kimmen_b@hotmail.com>
2023-10-19 19:28:40 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,22 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+################################################
+######### Ip adress Locator for fun :D #########
+################################################
+#### pip install geocoder ######################
+################################################
+import geocoder as geo
+
+# get your Own Ip Address
+Ip_address = geo.ip('me')
+
+# Find City of IP
+Ip = geo.ip('192.xx.xxx.x')
+Print(ip.city)
+
+# Get latitude and longitude of IP Address
+Print(ip.latlng)
+
+# Get Area Info
+Info = geo.google(‘City_name’)
+Print(info.geojson)
+Print(info.osm)
+Print(info.wkt)
