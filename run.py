00eac7bdf0af9fc1c4ac033a4e7165f548b1430c
Kim Bergstrom <kimmen_b@hotmail.com>
2023-10-19 21:13:27 +0000

    updated the structure and user input

--- run.py
+++ run.py
@@ -2,21 +2,34 @@
 ######### Ip adress Locator for fun :D #########
 ################################################
 #### pip install geocoder ######################
+#### github.com/DenisCarriere/geocoder #########
 ################################################
 import geocoder as geo
 
-# get your Own Ip Address
-Ip_address = geo.ip('me')
+while True:
 
-# Find City of IP
-Ip = geo.ip('192.xx.xxx.x')
-Print(ip.city)
+    # Input the IP address to look up
+    ip_address = input("Enter an IP address: ")
 
-# Get latitude and longitude of IP Address
-Print(ip.latlng)
+    # Find the location of the IP address
+    ip = geo.ip(ip_address)
 
-# Get Area Info
-Info = geo.google(‘City_name’)
-Print(info.geojson)
-Print(info.osm)
-Print(info.wkt)
+    # Check if the location is found
+    if ip.ok:
+        city = ip.city
+        country = ip.country
+        postal = ip.postal
+        org = ip.org
+        latlng = ip.latlng
+        location_name = ip.address
+
+        print(f"IP Address: {ip_address}")
+        print(f"Org.nr: {org}")
+        print(f"City: {city}")
+        print(f"Country: {country}")
+        print(f"Post.nr: {postal}")
+        print(f"Latitude and Longitude: {latlng}")
+        print(f"Location Name: {location_name}")
+    else:
+        print(f"Location not found for IP address: {ip_address} , Please try again")
+
